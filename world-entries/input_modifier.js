f59e23a9e1a661786e2a76eb36e10e08a3e7c282
Colin O'Neill <colin@faokryn.com>
2020-07-31 03:30:34 -0400

    Added scaffolding for commands

--- world-entries/input_modifier.js
+++ world-entries/input_modifier.js
@@ -8,14 +8,41 @@ const modifier = (text) => {
 
   if (text.startsWith('\n/') || text.startsWith('\n> You /') || text.startsWith('\n> You say "/')) {
 
-    const input = text.slice(text.indexOf('/') + 1).split('"');
     const currentEntries = worldEntries.flatMap(e => e.keys.split(', '));
+    const input = text.slice(text.indexOf('/') + 1).split('"').map(e => e.trim());
+
+    // account for 'Do' command appending a '.'
+    if (input[0].endsWith('.')) input[0] = input[0].slice(0, input[0].length - 1);
 
     // prevent output
     state.preventOutput = true;
     output = '';
 
-    state.message = JSON.stringify(currentEntries);
+    switch (input[0]) {
+      case 'listKeys':
+        state.message = 'listKeys';
+        break;
+      case 'showKey':
+        state.message = 'showKey';
+        break;
+      case 'addKey':
+        state.message = 'addKey';
+        break;
+      case 'removeKey':
+        state.message = 'removeKey';
+        break;
+      case 'modKey':
+        state.message = 'modKey';
+        break;
+      case 'appendKey':
+        state.message = 'appendKey';
+        break;
+      case 'aliasKey':
+        state.message = 'aliasKey';
+        break;
+      default:
+        state.message = 'default';
+    }
   }
 
   return {text: output};
