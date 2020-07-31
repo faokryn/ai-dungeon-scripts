e315ca5372543d26ae020cc8d84f6c73b3b70cba
Colin O'Neill <colin@faokryn.com>
2020-07-31 15:39:40 -0400

    Added /addKey command

--- world-entries/input_modifier.js
+++ world-entries/input_modifier.js
@@ -9,10 +9,10 @@ const modifier = (text) => {
   if (text.startsWith('\n/') || text.startsWith('\n> You /') || text.startsWith('\n> You say "/')) {
 
     const currentEntries = worldEntries.flatMap(e => e.keys.split(', '));
-    const input = text.slice(text.indexOf('/') + 1).split('"').map(e => e.trim());
+    const input = text.slice(text.indexOf('/') + 1).split('"');
 
-    // account for 'Do' command appending a '.'
-    if (input[0].endsWith('.')) input[0] = input[0].slice(0, input[0].length - 1);
+    input[0] = input[0].split(' ')[0]; // make sure command is one word, for more relevant error messages
+    if (input[0].endsWith('.')) input[0] = input[0].slice(0, input[0].length - 1); // account for 'Do' command appending a '.'
 
     // prevent output
     state.preventOutput = true;
@@ -28,7 +28,13 @@ const modifier = (text) => {
         else state.message = input[1] + ':\n\n' + worldEntries.filter(e => e.keys.includes(input[1])).map(e => e.entry).join('\n\n');
         break;
       case 'addKey':
-        state.message = 'addKey';
+        if (!input[1] || !input[2]) state.message = 'Usage: /addKey "<key>" entry';
+        else if (currentEntries.includes(input[1])) state.message = 'Key "' + input[1] + '" already exists! Use /modKey or /appendKey instead.';
+        else {
+          let entry = input.filter((e,i) => i > 1).join('"').trim();
+          addWorldEntry(input[1], entry);
+          state.message = input[1] + ':\n\n' + entry;
+        }
         break;
       case 'removeKey':
         state.message = 'removeKey';
