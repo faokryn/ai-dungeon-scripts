f690253cafcef23205c984252f231703d6933820
Colin O'Neill <colin@faokryn.com>
2020-07-31 15:58:15 -0400

    Added /removeKey command

--- world-entries/input_modifier.js
+++ world-entries/input_modifier.js
@@ -11,7 +11,7 @@ const modifier = (text) => {
     const currentEntries = worldEntries.flatMap(e => e.keys.split(', '));
     const input = text.slice(text.indexOf('/') + 1).split('"');
 
-    input[0] = input[0].split(' ')[0]; // make sure command is one word, for more relevant error messages
+    input[0] = input[0].split(' ')[0].trim(); // make sure command is one word, for more relevant error messages
     if (input[0].endsWith('.')) input[0] = input[0].slice(0, input[0].length - 1); // account for 'Do' command appending a '.'
 
     // prevent output
@@ -37,7 +37,23 @@ const modifier = (text) => {
         }
         break;
       case 'removeKey':
-        state.message = 'removeKey';
+        if (!input[1]) state.message = 'Usage: /removeKey "<key>"';
+        else if (!currentEntries.includes(input[1])) state.message = 'Key "' + input[1] + '" does not exist!';
+        else {
+          let entryCount = 0;
+          worldEntries.forEach((e, i) => {
+            if (e.keys.includes(input[1])) {
+              entryCount++;
+              if (entryCount === 1) {
+                if (e.keys === input[1]) removeWorldEntry(i);
+                else if (e.keys.startsWith(input[1])) updateWorldEntry(i, e.keys.replace(input[1] + ', ', ''), e.entry);
+                else if (e.keys.includes(input[1])) updateWorldEntry(i, e.keys.replace(', ' + input[1], ''), e.entry);
+              }
+            }
+          });
+          state.message = 'Key "' + input[1] + '" removed.';
+          if (entryCount > 1) state.message = 'WARNING: Multiple entries with key "' + input[1] + '". Only removed first.\n\n' + state.message;
+        }
         break;
       case 'modKey':
         state.message = 'modKey';
@@ -49,7 +65,7 @@ const modifier = (text) => {
         state.message = 'aliasKey';
         break;
       default:
-        state.message = 'default';
+        state.message = 'default:\n' + JSON.stringify(input);
     }
   }
 
